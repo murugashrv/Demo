9f23f39ba3e6899a2d6bdd9151ee748d80b18ab2
Murugan.V <Murugan.V@d01tmc013.dsrc.in>
2019-08-29 18:48:44 +0530

    TestPurpose cllass updateion

--- spring-boot-jpa-oracle-example/src/main/java/com/mkyong/TestPurpose.java
+++ spring-boot-jpa-oracle-example/src/main/java/com/mkyong/TestPurpose.java
@@ -2,4 +2,25 @@ package com.mkyong;
 
 public class TestPurpose {
 
+	private String id;
+	private int num;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	@Override
+	public String toString() {
+		return "TestPurpose [id=" + id + ", num=" + num + "]";
+	}
+	
+	
+	
 }
